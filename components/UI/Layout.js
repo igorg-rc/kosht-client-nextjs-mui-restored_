eb31be2de278d2729f42fdb0fcfbf701600e7d23
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2022-02-07 10:43:43 +0200

    add themeProvider to layout component

--- components/UI/Layout.js
+++ components/UI/Layout.js
@@ -3,23 +3,24 @@ import { MyContainer } from './UIUnits'
 import { Header } from './Header'
 import { RightMenu } from './RightMenu_'
 import LeftMenu from './LeftMenu'
-
+import { ThemeProvider } from '@mui/styles'
+import theme from "../../src/theme"
 
 export default function Layout({ children }) {
-  return <>
-  <MyContainer>
-    <Header />
-    <Grid container spacing={3}>
-      <Grid item xs={3}>
-        <LeftMenu />
+  return <ThemeProvider theme={theme}>
+    <MyContainer>
+      <Header />
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <LeftMenu />
+        </Grid>
+        <Grid item xs={6}>
+          {children}
+        </Grid>
+        <Grid item xs={3}>
+          <RightMenu />
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        {children}
-      </Grid>
-      <Grid item xs={3}>
-        <RightMenu />
-      </Grid>
-    </Grid>
-  </MyContainer>
-  </>
+    </MyContainer>
+  </ThemeProvider>
 }
